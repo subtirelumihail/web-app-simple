6fd866e33bd45b0ac7cae52abe358a9c515e4ef0
Subtirelu Mihail <dazzzz2007@gmail.com>
2014-07-23 10:20:01 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,1 @@
-/*=============================
-	Page Artist carousel
-=============================*/
 
-$('.slider').bxSlider({
-  minSlides: 1,
-  maxSlides: 3,
-  slideWidth: 400,
-  slideMargin: 10,
-  pager: false
-});
